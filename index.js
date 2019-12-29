70af9e7f899826ae8f4ba0541cbf75e7adb85bee
Aleck Sun <alecksun@gmail.com>
2019-12-29 11:29:34 +0000

    error base

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ module.exports = {
     routing: require('./lib/routing'),
     service: require('./lib/service'),
     using: require('./lib/using'),
+    serviceError: require('./lib/serviceError')
     middlewares: require('./lib/middlewares')
 }
